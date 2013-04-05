5489e967c9172c584e946dfcdd3fe8a86b2a3493
Tomas Neme <lacrymology@gmail.com>
2013-04-05 13:49:04 -0300

    add db module
    Signed-off-by: Tomas Neme <lacrymology@gmail.com>

---
+++ db.js
@@ -0,0 +1,10 @@
+exports = module.exports = function(config) {
+    //var conf = require('config').MediaDB;
+    var conf = config || {
+        dbName: "mediadb",
+        dbHost: "localhost",
+        dbPort: 27017
+    };
+    var db = require('mongoskin').db(conf.dbHost + ':' + conf.dbPort + '/' + conf.dbName + '?auto_reconnect', {safe:true});
+    return db;
+}
