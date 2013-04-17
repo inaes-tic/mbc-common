c46c5f6e8fed0db029e4ae80b2203b5f92d8fbad
José Luis Di Biase <josx@interorganic.com.ar>
2013-04-17 12:44:09 -0300

    Adding access to Config module
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- db/index.js
+++ db/index.js
@@ -1,9 +1,5 @@
 exports = module.exports = function(config) {
-    var conf = config || {
-        dbName: "mediadb",
-        dbHost: "localhost",
-        dbPort: 27017
-    };
+    var conf = config || require('config').Common.MediaDB;
     var db = require('mongoskin').db(conf.dbHost + ':' + conf.dbPort + '/' + conf.dbName + '?auto_reconnect', {safe:true});
     return db;
 }
