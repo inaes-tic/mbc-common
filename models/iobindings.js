eb563f86c83044765e49c477addd70f33b8178d2
José Luis Di Biase <josx@interorganic.com.ar>
2014-01-07 09:32:53 -0300

    Making pretty library requires
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- models/iobindings.js
+++ models/iobindings.js
@@ -1,10 +1,11 @@
-var    _ = require('underscore'),
-    config = require('config'),
+var _              = require('underscore'),
+    uuid           = require('node-uuid'),
+    backboneio     = require('backbone.io'),
+    config         = require('config'),
     search_options = config.Search,
-    collections = config.Common.Collections,
-    uuid = require('node-uuid'),
-    logger = mbc.logger().addLogger('iobindings'),
-    backboneio = require('backbone.io');
+    collections    = config.Common.Collections,
+    logger         = require('../logger')().addLogger('iobindings')
+;
 
 // Override mongoStore read method with custom
 var searchWrapper = require('./searchWrapper.js');
