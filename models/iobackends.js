5bd4604cfeeebfe758c0bff1e1ac64c448501a23
Adrian Pardini <publico@tangopardo.com.ar>
2014-01-15 06:40:36 -0300

    adds a redis middleware

--- models/iobackends.js
+++ models/iobackends.js
@@ -5,11 +5,82 @@ var _              = require('underscore'),
     search_options = config.Search,
     collections    = config.Common.Collections,
     logger         = require("../logger")().addLogger('iobackends')
-    publisher      = require('../pubsub')();
+    publisher      = require('../pubsub')(),
+    listener       = require('../pubsub')();
 ;
 
 // Override mongoStore read method with custom
 var searchWrapper = require('./searchWrapper.js');
+
+// copypasta from backbone.io/lib/browser.js
+function inherits(Parent, Child, mixins) {
+    var Func = function() {};
+    Func.prototype = Parent.prototype;
+
+    mixins || (mixins = [])
+    _.each(mixins, function(mixin) {
+        _.extend(Func.prototype, mixin);
+    });
+
+    Child.prototype = new Func();
+    Child.prototype.constructor = Child;
+
+    return _.extend(Child, Parent);
+};
+
+// stupid way to not process our own redis messages.
+var _client_id = uuid.v4();
+
+function makeRedisMiddleware(backend, name, chain) {
+    // smelly code, smelly code... It's not your fault...
+    var _chan = '_RedisSync.' + name;
+    logger.error('_chan: ', _chan);
+    var io = backend.io;
+
+    function _onMessage(chan, msg) {
+        if (chan != _chan) {
+            return;
+        }
+
+        if (msg._redis_source == _client_id) {
+            return;
+        }
+
+        var res = {end:function(){}, error:function(){}};
+        var req = msg;
+
+        io.handle (req, res, function(err, result) {
+            if (err) {
+                logger.error('RedisSync _onMessage: ', err, result);
+            }
+        });
+
+        var event = {create: 'created', read: 'updated', update: 'updated', delete: 'removed'};
+        io.emit(event[req.method], req.model);
+        io.emit('redis', req.method, req.model);
+        io.emit('redis:'+req.method, req.model);
+    };
+
+    listener.subscribe(_chan);
+    listener.on('JSONmessage', _onMessage);
+
+    function _middleware(req, res, next) {
+        if (req._redis_source) {
+            if (chain) {
+                next();
+            } else {
+                res.end(req.model);
+            }
+            return;
+        }
+        if (req.method.match(/create|update|delete/)) {
+            publisher.publishJSON(_chan, { model: req.model, method:req.method, _redis_source:_client_id});
+        }
+        next();
+    };
+
+    return _middleware;
+};
 
 var iobackends = module.exports = exports = function (db, backends) {
     var self = this;
@@ -46,13 +117,45 @@ var iobackends = module.exports = exports = function (db, backends) {
 
     /* process the backends object to streamline code */
     var binded = [];
-    _(this.backends).each (function (backend) {
+    _(this.backends).each (function (backend, name) {
         backend.io = backboneio.createBackend();
+
+        var head = [];
+        var tail;
+
+        // Ok, so this is the deal:
+        //
+        // Some middlewares like memoryStore, configStore and mongo do a res.end().
+        // This is not bad as normally we put them on the end of the chain (and it
+        // kind of make sense as they imply that the data is persisted somewhere).
+        //
+        // However, here by default we append debug and it is not called on those
+        // cases.
+        //
+        // Also, sometimes we want to cut the processing and avoid hitting the
+        // underlying storage and as such need to put something in between.
+        // (Like, when we get an update from Redis and the other side already saved
+        // to Mongo)
+        // Thus, the splitting.
         if (backend.use) {
-            _(backend.use).each (function (usefn) {
-                backend.io.use(usefn);
-            });
+            tail = backend.use.pop();
+            head = backend.use;
         }
+        _(head).each (function (usefn) {
+            backend.io.use(usefn);
+        });
+
+        backend.io.use (self.middleware.debug);
+
+        if (backend.redisSync) {
+            var _middleware = makeRedisMiddleware(backend, name, backend.redisChain);
+            backend.io.use(_middleware);
+        }
+
+        if (tail) {
+            backend.io.use(tail);
+        }
+
         if (backend.mongo) {
             var mongo = _.extend ({db: db, opts: {}}, backend.mongo);
             var fn = _.identity;
@@ -65,7 +168,6 @@ var iobackends = module.exports = exports = function (db, backends) {
                                                     mongo.collection,
                                                     mongo.opts)));
         }
-        backend.io.use (self.middleware.debug);
     });
 
     logger.info ('binding to mongo collections:', binded.join(', ') + '.');
