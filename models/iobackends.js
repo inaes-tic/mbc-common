e6a0da7caf465456e3259b62680b4ca71c708de7
Adrian Pardini <publico@tangopardo.com.ar>
2014-01-20 13:33:51 -0300

    iobackends: adds redisMiddleware from iocompat if requested.

--- models/iobackends.js
+++ models/iobackends.js
@@ -47,7 +47,7 @@ var iobackends = module.exports = exports = function (db, backends) {
 
     /* process the backends object to streamline code */
     var binded = [];
-    _(this.backends).each (function (backend) {
+    _(this.backends).each (function (backend, name) {
         backend.io = backboneio.createBackend();
         if (backend.use) {
             _(backend.use).each (function (usefn) {
@@ -64,6 +64,14 @@ var iobackends = module.exports = exports = function (db, backends) {
          * from the browser as events so we can react and update our models.
          */
         backend.io.use (iocompat.eventMiddleware(backend));
+
+        /*
+         * adds the redis link layer middleware that listens for changes on
+         * other servers and also broadcasts ours.
+         */
+        if (backend.redis) {
+            backend.io.use (iocompat.redisMiddleware(backend, name, backend.redis.chain));
+        }
 
         /*
          * On the backend definition we either pass a 'mongo' hash with the
