f0a263460e3f8cfa2d057cae433b81c58d97783b
Adrian Pardini <publico@tangopardo.com.ar>
2014-02-13 20:54:27 -0300

    avahi: emit signals when services come up or dissapear.

--- avahi/index.js
+++ avahi/index.js
@@ -1,6 +1,8 @@
-var mdns   = require('mdns2'),
-    uuid   = require('node-uuid'),
-    logger = require("../logger")().addLogger('avahi')
+var mdns     = require('mdns2'),
+    uuid     = require('node-uuid'),
+    logger   = require("../logger")().addLogger('avahi'),
+    Backbone = require("backbone"),
+    _        = require("underscore")
 ;
 
 /*
@@ -9,14 +11,38 @@ var mdns   = require('mdns2'),
  * client_id is an unique token used to identify this particular instance. If
  * not given, an uuid is created.
  * namespace is the prefix used to distinguish the services. Defaults to 'MBC'
+ *
+ * Properties:
+ *  services
+ *      A hash that maps from a service name to its properties.
+ *
+ * Signals:
+ *  serviceUp(service):
+ *      'service' is an object with keys for host, port, name, addresses and txtRecord.
+ *
+ *  serviceDown(service_name)
  */
 var avahi = exports = module.exports = function (client_id, namespace) {
     var self = this;
+    _.extend(self, Backbone.Events);
 
     self.client_id = client_id || uuid.v4();
     self.namespace = namespace || 'MBC';
 
     self.services = {};
+
+    self.browser = mdns.createBrowser(mdns.tcp(self.namespace));
+    self.browser.on('serviceUp', function(service) {
+        var ret = _.pick(service, 'host', 'port', 'name', 'addresses', 'txtRecord');
+        self.services[ret.name] = ret;
+        self.trigger('serviceUp', ret)
+    });
+    self.browser.on('serviceDown', function(service) {
+        delete self.services[service.name];
+        self.trigger('serviceDown', service.name)
+    });
+
+    self.browser.start();
 
     return self;
 };
